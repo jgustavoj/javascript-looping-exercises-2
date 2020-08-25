b850724a5ea5f9c5492cc915ff11b2eab1277200
jgustavoj <gjime011@fiu.edu>
2020-08-25 01:43:33 +0000

    Exercise 13

--- .breathecode/resets/13-Merge-arrays/app.js
+++ .breathecode/resets/13-Merge-arrays/app.js
@@ -1,15 +1,8 @@
 var chunk_one = [ 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell' ];
 var chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas'];
-var new_array = [];
+
 const mergeArrays = (firstArray, secondArray) => {
-    //your code here
-    for ( let i = 0; i < firstArray.length; i++){
-        new_array.push(firstArray[i]);
-    }
-    for ( let j = 0; j < secondArray.length; j++){
-        new_array.push(secondArray[j]); 
-    }
-    return new_array;
+	//your code here
 }
 
 console.log(mergeArrays(chunk_one, chunk_two));
