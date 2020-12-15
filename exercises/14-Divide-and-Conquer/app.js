1230625baa3bafcaf5c9c50c134c04b588d16596
jgustavoj <gjime011@fiu.edu>
2020-12-15 05:31:33 +0000

    update

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,21 +1,34 @@
 let list_of_numbers = [4,	80,	85,	59,	37,25,	5,	64,	66,	81,20,	64,	41,	22,	76,76,	55,	96,	2,	68];
-let odd = [];
-let even = [];
-let newArray = []; 
+
 // your code here
-function mergeTwoList (arr){
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] % 2 === 0){
+// your code here
+//create function with function then name of function 
+//create variable with empty array called odd in order to push odd numbers into 
+//create variable with empty array called even in order to push even numbers into 
+//create a for loop that iterates through the list of numbers array
+//create conditional statement with if the value 
+//if statements pushes odd into even array 
+//else statement pushes even into even array
+//
+
+let odd =[];
+let even = [];
+let newArray= [];
+let mergeTwoList = (arr) => {    
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] %2 == 0 ){
             even.push(arr[i]);
-        }
-        else{
+        } else{
             odd.push(arr[i]);
         }
     }
+    //console.log(odd);
+    //console.log(even);
     newArray.push(odd);
-    newArray.push(even); 
-    console.log(newArray); 
+    newArray.push(even);
+    console.log(newArray);
+
 }
 
+mergeTwoList(list_of_numbers)
 
-console.log(mergeTwoList(list_of_numbers))
--- exercises/19-And-One-and-a-Two-and-a-Three/app.js
+++ exercises/19-And-One-and-a-Two-and-a-Three/app.js
@@ -3,23 +3,10 @@ contact = {
     phone: "321-321-4321",
     email: "test@test.com"
 }
-
+let value = " ";
 for(key in contact){
-    contact.push(key + ":" + contact[key])
-    console.log(contact);
-    
+    value += key + " : " + contact[key] + " "; 
+    // Code goes here
 }
 
-
-// var person = {fname:"John", lname:"Doe", age:25}; 
-  
-//   var text = "";
-//   var x;
-//   for (x in person) {
-// //     text += person[x] + " ";
-
-//  let _output = []
-//     for(let _key in _contact){
-//     // Code goes here
-//       _output.push(_key + " : " + _contact[_key])
-//     }
+console.log(value);
